368f6b8983b9c3822027d204293e4da4b6fbc36a
lonchanick <diego.arroyo2462050@gmail.com>
2025-06-30 13:13:58 -0400

    lab for learning purposes

--- Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
+++ Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
@@ -1,30 +1,12 @@
 
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
+const App = (props)=> {
+  const {counter} = props;
 
-const App = () => {
-
-  const elPatron = {
-  header: 'Half Stack application development',
-
-  parts : [
-    {courseName: 'Fundamentals of React', exercises: 10, key: 1},
-    {courseName: 'Using props to pass data', exercises: 7, key: 2},
-    {courseName: 'State of a component', exercises: 14, key: 3}],
-
-  total: 31
-  }
-  
-  
-  return (
-    <div>
-      <code>Wellcome to the exercise 1.5</code>
-      <Header header = {elPatron.header}/> 
-      <Content thisShouldBeTheNameOfTheParamRecivedInTheComponent = {elPatron.parts} /> 
-      <Total total = {elPatron.total}/>
-    </div>
+  return(
+    <>
+      <p>{counter}</p>
+    </>
   )
 }
 
-export default App
+export default App;
--- Part1/1.2Course-Information-step-1 - Copy/newApp/src/main.jsx
+++ Part1/1.2Course-Information-step-1 - Copy/newApp/src/main.jsx
@@ -2,4 +2,8 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter_JsCode = 1;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter = {counter_JsCode} />
+)
