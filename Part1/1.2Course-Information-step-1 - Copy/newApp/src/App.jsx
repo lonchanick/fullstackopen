e7a12566e9a2a8b758dc3087b83cdb3c9be8cfa3
lonchanick <diego.arroyo2462050@gmail.com>
2025-06-30 12:13:16 -0400

    exercise1.5 done

--- Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
+++ Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
@@ -4,21 +4,25 @@ import Content from './Content'
 import Total from './Total'
 
 const App = () => {
-  const header = 'Half Stack application development'; 
 
-  const parts = [
+  const elPatron = {
+  header: 'Half Stack application development',
+
+  parts : [
     {courseName: 'Fundamentals of React', exercises: 10, key: 1},
     {courseName: 'Using props to pass data', exercises: 7, key: 2},
-    {courseName: 'State of a component', exercises: 14, key: 3}]
+    {courseName: 'State of a component', exercises: 14, key: 3}],
 
-  const total = 31
+  total: 31
+  }
+  
   
   return (
     <div>
-      <code>Wellcome to the exercise 1.4</code>
-      <Header header = {header}/> 
-      <Content thisShouldBeTheNameOfTheParamRecivedInTheComponent = {parts} /> 
-      <Total total = {total}/>
+      <code>Wellcome to the exercise 1.5</code>
+      <Header header = {elPatron.header}/> 
+      <Content thisShouldBeTheNameOfTheParamRecivedInTheComponent = {elPatron.parts} /> 
+      <Total total = {elPatron.total}/>
     </div>
   )
 }
