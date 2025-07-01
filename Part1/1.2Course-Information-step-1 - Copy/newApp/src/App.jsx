8e69e5c4e52bc11cb1844a7fceb2f6da65575af1
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-01 12:33:57 -0400

    lab

--- Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
+++ Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
@@ -1,12 +1,35 @@
 
-const App = (props)=> {
-  const {counter} = props;
+import {useState} from 'react'
+import Display from './Display';
+import Button from './Button';
+
+const App = () =>{
+  const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value', counter)
+  
+  const increase = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter+1)
+  }
+
+  const decrease = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter-1)
+  }
+
+  const resetCounter = ()=> {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  } 
 
   return(
-    <>
-      <p>{counter}</p>
+    <> 
+    <Display counter = {counter} />
+    <Button onClick = {increase} text = 'Increase' /> 
+    <Button onClick = {resetCounter} text = 'Reset' /> 
+    <Button onClick = {decrease} text = 'decrease' /> 
     </>
-  )
+  );
 }
 
-export default App;
+export default App
--- Part1/1.2Course-Information-step-1 - Copy/newApp/src/main.jsx
+++ Part1/1.2Course-Information-step-1 - Copy/newApp/src/main.jsx
@@ -1,9 +1,7 @@
-import ReactDOM from 'react-dom/client'
+import reactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter_JsCode = 1;
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter = {counter_JsCode} />
+reactDOM.createRoot(document.getElementById('root')).render(
+    <App counter ={22} />
 )
