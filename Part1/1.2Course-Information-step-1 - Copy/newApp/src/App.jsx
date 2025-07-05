c26fe91a4dd5ba3f472191b47271ea68c97ca425
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-05 09:40:08 -0400

    exercise 1.6 done

--- Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
+++ Part1/1.2Course-Information-step-1 - Copy/newApp/src/App.jsx
@@ -2,19 +2,25 @@ import {useState} from 'react'
 import Button from './Button'
  
 const App = () =>{
-  const [val, setVal] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const clickHandler = (num)=> setVal(num)
+  const evHandSetGood = (val)=> setGood(val);
+  const evHandSetNeutral = (val)=> setNeutral(val);
+  const evHandSetBad = (val)=> setBad(val);
 
   return(
     <>
-    <p>{val}</p>
-    <Button fnReference = {()=> clickHandler(1000)} text = 'Set to 1.000' /><br></br>
-    {/* <button onClick = {()=>clickHandler(1000)} >Set to 1.000</button> */}
-    <Button fnReference = {()=> clickHandler(0)} text = 'Set to 0' /><br></br>
-    {/* <button onClick = {()=>clickHandler(0)} >Set to 0</button> */}
-    <Button fnReference = {()=> clickHandler(val+1)} text = 'Increment' /><br></br>
-    {/* <button onClick = {()=>clickHandler(val+1)} >Increment + 1</button> */}
+    <code>Exercise 1.6</code>
+    <h1>Give feedback</h1>
+    <Button fnReference={()=>evHandSetGood(good +1)} text = 'Good'/>
+    <Button fnReference={()=>evHandSetNeutral(neutral +1)} text = 'Neutral'/>
+    <Button fnReference={()=>evHandSetBad(bad +1)} text = 'Bad'/>
+    <h2>Statistics</h2>
+    <p><span>Good:</span><span>{good}</span></p>
+    <p><span>Neutral:</span><span>{neutral}</span></p>
+    <p><span>Bad:</span><span>{bad}</span></p> 
     </>
   )
 }
