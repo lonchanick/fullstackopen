63f8579e736d1f86c5748ae359d4a7e942a78e77
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-10 17:52:46 -0400

    exercise2.1 done

---
+++ part2/myapp/src/components/Course.jsx
@@ -0,0 +1,14 @@
+
+const Course = ({course})=>{ 
+    // console.log(course)
+    const {id,name,parts} = course;
+    return(
+        <div>
+            <span><code>exercise 2.1</code></span>
+            <h1 key={id}>{name}</h1>
+            {parts.map(el => <p key={el.id}>{el.name}</p>)}
+        </div>
+    )
+}
+
+export default Course
