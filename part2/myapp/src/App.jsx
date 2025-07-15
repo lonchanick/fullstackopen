bd79b9a3f10ef9571209cc95e1d791de5c7aaa80
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-15 12:27:17 -0400

    exercise 2.7 done

--- part2/myapp/src/App.jsx
+++ part2/myapp/src/App.jsx
@@ -1,79 +1,45 @@
-
-import Course from './components/Course'
-
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-export const Total = ({course}) => {
-  const ex = course.map(el => el.exercises) 
-  return <strong>Total of {ex.reduce((acc,n)=> acc+n)} exercises.</strong>
-}
+import { useState } from "react";
 
 const App = () => {
-  const courses = [
+  const [person, setNewPerson] = useState([{ id: 0, name: "Agustin" }]);
+  const [newPersonInput, setNewPersonInput] = useState("");
+
+  const onChangeNewPersonInput = (event) => {
+    console.log(event.target.value);
+    setNewPersonInput(event.target.value)
+  };
+
+  const onSubmitForm = (event)=>{
+    event.preventDefault();
+    const alreadyExist = person.find( p=> p.name === newPersonInput);
+    
+    if(alreadyExist)
     {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+      alert(`${newPersonInput} already exist!`);
+      return;
     }
-  ]
+    const newPerson = {
+      id: person.length + 1,
+      name: newPersonInput
+    }
+    setNewPerson(person.concat(newPerson));
+    setNewPersonInput('');
+  }
 
   return (
     <>
-      <span><code>exercise 2.5</code></span>
-      {courses.map(c => <Course key={c.id} course={c} /> )} 
+      <h2>PhoneBook</h2>
+      <form onSubmit={onSubmitForm}>
+        Name: <input value={newPersonInput} onChange={onChangeNewPersonInput} />
+        <button type="submit">Submit</button>
+      </form>
+      
+      <h2>Numbers</h2>
+      {person.map((person) => (
+        <p key={person.id}>{person.name}</p>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App 
+export default App;
