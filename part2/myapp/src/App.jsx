4d06811caac1e45365c0053935afb3e7c2a061fa
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-15 12:34:32 -0400

    exercise 2.8 done

--- part2/myapp/src/App.jsx
+++ part2/myapp/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  const [person, setNewPerson] = useState([{ id: 0, name: "Agustin" }]);
+  const [person, setNewPerson] = useState([{ id: 0, name: "Agustin", number: "+593 2462050" }]);
   const [newPersonInput, setNewPersonInput] = useState("");
+  const [newNumberInput, setNewNumberInput] = useState("");
 
   const onChangeNewPersonInput = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setNewPersonInput(event.target.value)
+  };
+  const onChangeNewNumberInput = (event) => {
+    // console.log(event.target.value);
+    setNewNumberInput(event.target.value)
   };
 
   const onSubmitForm = (event)=>{
@@ -20,23 +25,26 @@ const App = () => {
     }
     const newPerson = {
       id: person.length + 1,
-      name: newPersonInput
+      name: newPersonInput,
+      number: newNumberInput
     }
     setNewPerson(person.concat(newPerson));
     setNewPersonInput('');
+    setNewNumberInput('');
   }
 
   return (
     <>
       <h2>PhoneBook</h2>
       <form onSubmit={onSubmitForm}>
-        Name: <input value={newPersonInput} onChange={onChangeNewPersonInput} />
+        Name: <input value={newPersonInput} onChange={onChangeNewPersonInput} /><br></br>
+        Number: <input value={newNumberInput} onChange={onChangeNewNumberInput} /><br></br>
         <button type="submit">Submit</button>
       </form>
       
       <h2>Numbers</h2>
       {person.map((person) => (
-        <p key={person.id}>{person.name}</p>
+        <p key={person.id}>{person.name} {person.number}</p>
       ))}
     </>
   );
