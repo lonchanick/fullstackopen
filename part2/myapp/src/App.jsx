c4271e6e9bad4e0ac79e619af78909aa95093076
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-23 11:29:11 -0400

    good enought LAB job  4 2day

--- part2/myapp/src/App.jsx
+++ part2/myapp/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react' 
-import Note from './components/Note'
-import noteService  from './services/notes'
+import { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from "./services/notes";
 
 export const Total = ({ course }) => {
   const ex = course.map((el) => el.exercises);
@@ -12,51 +12,58 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewnote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
-  
-  useEffect(() => {  
-      noteService.getAll().then(response => setNotes(response));
-    }, []) 
-  
-  const notesToShow = showAll ? notes : notes.filter(note => note.important); 
+
+  useEffect(() => {
+    const nonExistingOb = {
+      "id": "903a007",
+      "content": "Non Existing Obj",
+      "important": false
+    }
+    noteService
+    .getAll()
+    .then((response) => setNotes(response.concat(nonExistingOb)));
+  }, []);
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   const addNote = (event) => {
     event.preventDefault();
-    
-    if(newNote === '') 
-      return;
+
+    if (newNote === "") return;
 
     const newNoteObj = {
       //id: notes.length+1,
       content: newNote,
-      important: Math.random() > 0.5
-    }
+      important: Math.random() > 0.5,
+    };
 
-    noteService.create(newNoteObj)
-    .then(resp => { 
+    noteService.create(newNoteObj).then((resp) => {
       setNotes(notes.concat(resp));
-      setNewnote('');
+      setNewnote("");
     });
   };
 
-  const toggleImportance = (id)=>
-  {
-    const note = notes.find(n => n.id === id); 
-    const changedNote = {...note, important: !note.important};
+  const toggleImportance = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
 
-    noteService.update(id,changedNote)
-    .then((data)=>{
-      setNotes(notes.map(n => n.id === id ? data : n))
+    noteService.update(id, changedNote)
+    .then((data) => {
+      setNotes(notes.map((n) => (n.id === id ? data : n)));
     })
-  }
+    .catch(error => {
+      alert(`something went wrong! ${error}`)
+      setNotes(notes.filter(n => n.id !== id))
+    });
+  };
 
-  const deleteNote = (id)=> {
-    noteService.remove(id)
-    .then(resp => {
+  const deleteNote = (id) => {
+    noteService.remove(id).then((resp) => {
       //nota: no se re-renderiza la pantalla
       console.log(resp);
-      const newArray = notes.filter(n => n.id !== resp)
+      const newArray = notes.filter((n) => n.id !== resp);
       setNotes(newArray);
-    }); 
+    });
   };
 
   const handlerNoteChange = (event) => {
@@ -69,19 +76,24 @@ const App = () => {
         <code>LAB</code>
       </span>
       <span>
-      <h1>Notes App</h1>
-      <button onClick={()=>setShowAll(!showAll)}>{showAll ? 'Important':'All'}</button>
-      </span> 
+        <h1>Notes App</h1>
+        <button onClick={() => setShowAll(!showAll)}>
+          {showAll ? "Important" : "All"}
+        </button>
+      </span>
 
-      {notesToShow.map(note => <Note 
-      key={note.id} 
-      note={note} 
-      toggleImportance={toggleImportance} 
-      deleteNote={deleteNote}/>)}
+      {notesToShow.map((note) => (
+        <Note
+          key={note.id}
+          note={note}
+          toggleImportance={toggleImportance}
+          deleteNote={deleteNote}
+        />
+      ))}
 
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handlerNoteChange} />
-        
+
         <button type="submit">submit</button>
       </form>
     </>
