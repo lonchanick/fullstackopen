449f526f33cf7ca2d0be815b9712367d858de239
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-28 11:52:21 -0400

    done

--- part2/myapp/src/App.jsx
+++ part2/myapp/src/App.jsx
@@ -1,110 +1,44 @@
+//do it again!
 import { useState, useEffect } from "react";
-import Note from "./components/Note";
-import noteService from "./services/notes";
 
-import './index.css'
-import ErrNotification from './components/ErrNotification'
-import Footer from './components/Footer'
-
-export const Total = ({ course }) => {
-  const ex = course.map((el) => el.exercises);
-  return <strong>Total of {ex.reduce((acc, n) => acc + n)} exercises.</strong>;
-};
+import "./index.css";
+import Footer from "./components/Footer";
+import axios from 'axios'
 
 const App = () => {
-  //states; a piece of state x 3
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewnote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('some error here');
+  const [searchValue, setSerchValue] = useState("");
+  const [currecy, setCurrency] = useState(null);
+  const [currencyRateSet, setCurrencyRate] = useState([{}]);
+
+  const serachValueHandler = (e) => setSerchValue(e.target.value);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    setCurrency(searchValue);
+  };
 
   useEffect(() => {
-    const nonExistingOb = {
-      "id": "903a007",
-      "content": "Non Existing Obj",
-      "important": false
+    console.log('current value of SearchValue: ', searchValue);
+    if(currecy)
+    {
+      axios.get(`https://open.er-api.com/v6/latest/${currecy}`)
+      .then(response =>{
+        setCurrencyRate(response.data.rates)
+      });
     }
-    noteService
-    .getAll()
-    .then((response) => setNotes(response.concat(nonExistingOb)));
-  }, []);
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
-
-  const addNote = (event) => {
-    event.preventDefault();
-
-    if (newNote === "") return;
-
-    const newNoteObj = {
-      //id: notes.length+1,
-      content: newNote,
-      important: Math.random() > 0.5,
-    };
-
-    noteService.create(newNoteObj).then((resp) => {
-      setNotes(notes.concat(resp));
-      setNewnote("");
-    });
-  };
-
-  const toggleImportance = (id) => {
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
-
-    noteService.update(id, changedNote)
-    .then((data) => {
-      setNotes(notes.map((n) => (n.id === id ? data : n)));
-    })
-    .catch(() => {
-      setErrorMessage(`${note.content} was already removed from server`);
-      setTimeout(()=>{
-        setErrorMessage(null)
-      }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    });
-  };
-
-  const deleteNote = (id) => {
-    noteService.remove(id).then((resp) => {
-      //nota: no se re-renderiza la pantalla
-      console.log(resp);
-      const newArray = notes.filter((n) => n.id !== resp);
-      setNotes(newArray);
-    });
-  };
-
-  const handlerNoteChange = (event) => {
-    setNewnote(event.target.value);
-  };
+  }, [currecy]);
 
   return (
     <>
-      <span>
-        <code>LAB</code>
-        <ErrNotification message ={errorMessage}></ErrNotification>
-      </span>
-      <span>
-        <h1>Notes App</h1>
-        <button onClick={() => setShowAll(!showAll)}>
-          {showAll ? "Important" : "All"}
-        </button>
-      </span>
-
-      {notesToShow.map((note) => (
-        <Note
-          key={note.id}
-          note={note}
-          toggleImportance={toggleImportance}
-          deleteNote={deleteNote}
-        />
-      ))}
-
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handlerNoteChange} />
-
-        <button type="submit">submit</button>
+      <h1>Currency Exchange Rate</h1>
+      <form onSubmit={onSubmitForm}>
+        Currency: <input value={searchValue} onChange={serachValueHandler} />
+        <button type="submit">Submit</button>
       </form>
+      <pre>
+        {JSON.stringify(currencyRateSet, null, 2)}
+      </pre>
       <Footer />
     </>
   );
