b3becdb83a3a8eadb1ded91fe38684a8a9d6d6ba
lonchanick <diego.arroyo2462050@gmail.com>
2025-07-30 19:29:27 -0400

    exercise 2.20 done!

--- part2/myapp/src/App.jsx
+++ part2/myapp/src/App.jsx
@@ -1,41 +1,66 @@
-import {useState, useEffect} from 'react'
-import Footer from './components/Footer'
-import axios from 'axios'
-import CountryInfo from './components/CountryInfo';
-
- 
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import axios from "axios";
+import CountryInfo from "./components/CountryInfo";
 
 const App = () => {
-  const [countryField, setCountryField] = useState("");
-  const [countries, setCountries] = useState([]);
+  const [countryField, setCountryField] = useState(""); //para actualizar el campo country y filtrar
+  const [countries, setCountries] = useState([]); //para recuperar todas las countries una sola ves
+  const [showCountry, setShowCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  const onChCountryField = (e) => {
+    setCountryField(e.target.value);
+    setShowCountry(null);
+  };
+
+  const filteredCountry = countryField
+    ? countries.filter((c) =>
+        c.name.common.toLowerCase().startsWith(countryField.toLowerCase())
+      )
+    : [];
 
 
-  const onChCountryField = (e)=> setCountryField(e.target.value);
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setCountries(response.data);
+      });
 
-  const filteredCountry = countryField ? countries
-  .filter(c => c.name.common.toLowerCase().startsWith(countryField.toLowerCase()))
-  : [];
+    // weather exercise
+    const API_KEY = "32fc5765dacfc1f18038cd9a5a7ff1b9";
+    const city = "Curico";
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const the_bare_necessities = {
+          temperature: data.main.temp,
+          icon: data.weather[0].icon,
+          wind: data.wind
+        };
+        setWeather(the_bare_necessities);
+      })
+      .catch((error) => console.error("Error fetching weather:", error)); 
+  }, []);
 
-  // console.log("filtered ctr: ",filteredCountry)
-
-  useEffect(()=>{
-    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-    .then(response => {
-      setCountries(response.data); 
-    })
-  }, [])
-
-  if(countries.length === 0) return (<p>Loading Countries!</p>);
+  if (countries.length === 0) return <p>Loading Countries!</p>;
 
   return (
     <>
       <h1>Country Filterer!</h1>
       <form>
-        Country: <input value={countryField} onChange={onChCountryField} /> 
+        Country: <input value={countryField} onChange={onChCountryField} />
       </form>
       <div>
-        <CountryInfo country = {filteredCountry} />
-      </div> 
+        <CountryInfo
+          country={showCountry || filteredCountry}
+          setShowCountry={setShowCountry}
+          weather={weather}
+        />
+      </div>
       <Footer />
     </>
   );
--- part2/myapp/src/components/CountryInfo.jsx
+++ part2/myapp/src/components/CountryInfo.jsx
@@ -1,14 +1,16 @@
-const CountryInfo = ({country}) => {
+const CountryInfo = ({country, setShowCountry, weather}) => { 
   if(country)
   {
-    if(country.length > 10)
+
+    if(country.length > 10){
       return (<p>Too many matches, specify another filter.</p>);
-    else if(country.length === 1)
-    {
+    }
+    
+    if(country.length === 1){ 
       return (
       <div>
         {/* {console.log(country)} */} 
-        <h2>{country[0].name.common}</h2>
+        <h2>{country[0].name.official}</h2>
         <p>Capital: {country[0].capital}</p>
         <p>Area: {country[0].area}</p>
         <h2>Lang</h2>
@@ -23,14 +25,25 @@ const CountryInfo = ({country}) => {
         alt={`Flag of ${country[0].name.common}`}
         style={{ width: "200px" }}
         />
+        <div>
+          <h2>Weather in {country[0].capital}, {country[0].name.common}</h2> 
+          Temperature: {weather.temperature}
+          <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}></img>
+          <p>Wind Speed{` ${weather.wind.speed} `} m/s</p>
+        </div>
       </div>);
-    }
-    else
-      return (country.map(c => <li key={c.name.common}>{c.name.common}</li>));
-  }
-  else
-  {
-    return(<p>no Country content</p>);
+    } 
+    
+    return (country.map((c) => {
+        return (
+        <li key={c.name.common}>{c.name.common}{" "}
+        <button onClick={()=> { 
+          setShowCountry([c]);
+        }}>Show</button></li>);
+      }));
+
+  }else{
+    return(<p>No Country Info</p>);
   }
 };
 
