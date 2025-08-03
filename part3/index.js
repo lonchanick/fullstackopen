1d2e114fd4167bf50ff64f8cc253f8d11d8aab78
lonchanick <diego.arroyo2462050@gmail.com>
2025-08-03 14:34:10 -0400

    part 3 theory

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -18,16 +19,60 @@ let notes = [
     important: true,
   },
 ];
-
+/********************/
+/*NOTES APP REQUEST*/
+/********************/
+//GET HOME PAGE
 app.get('/', (reques,response)=> response.send('<h1>home</h1>'))
-app.get('/notes',(request,response)=> response.json(notes));
-app.get('/notes/:id', (request, response)=>{
+
+//GET ALL NOTES
+app.get('/api/notes',(request,response)=> response.json(notes));
+
+//GET NOTE BY ID
+app.get('/api/notes/:id', (request, response)=>{
   const id = request.params.id;
   const obj = notes.find(el => el.id === id);
   if(obj)
     response.json(obj);
 
   response.status(404).end();
+})
+
+//DELETE NOTE BY ID
+app.delete('/api/notes/:id', (request, response)=>{ 
+  const id = request.params.id;
+  notes = notes.filter(n => n.id !== id);
+  // response.json(notes_filtered);
+  response.json(notes);
+})
+
+//CREATE NEW NOTE
+const createId = ()=>{
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => Number(n.id)))
+  : 0;
+
+  const totalId = String(maxId + 1);
+
+  return totalId;
+}
+
+app.post('/api/notes', (req, resp)=>{
+  
+  const body = req.body
+  if(!body.content)
+    return resp.status(400).json({"error": "Content missing"})
+
+  const newNote = {
+    id: createId(),
+    content: body.content,
+    important: body.important || false
+  } 
+
+  console.log('New note to add:',newNote);
+  notes = notes.concat(newNote);
+  resp.json(notes);
+
 })
 
 
