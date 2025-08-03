c17b70d393b78e3cdbe3863d1474fa01e7979540
lonchanick <diego.arroyo2462050@gmail.com>
2025-08-03 15:45:22 -0400

    exercise 3.2 done: Implement a page at the address http://localhost:3001/info

--- part3/index.js
+++ part3/index.js
@@ -1,41 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 //home page
-app.get('/api', (request, response)=>{
-    response.json({'Home content':"This is the Home Page"})
-})
+app.get("/api", (request, response) => {
+  response.json({ "Home content": "This is the Home Page" });
+});
 
 //get persons
-app.get('/api/persons',(request, response)=>{
-    response.status(200).json(persons);
-})
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(persons);
+});
 
+//get info
+app.get("/api/info", (request, response) => {
+  const today = new Date();
+  const formatted = today.toLocaleDateString("en-US", {
+    weekday: "long", // e.g., Saturday
+    year: "numeric",
+    month: "long", // e.g., August
+    day: "numeric",
+  });
+  const info = `${formatted} --- Phone Book has info for ${persons.length} people.`;
+  response.status(200).json(info);
+});
 
 const PORT = 3001;
-app.listen(PORT, ()=>{
-    console.log(`App running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
