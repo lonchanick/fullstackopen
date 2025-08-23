33d5de63771602b6b93d9b42870fbd6b1099a398
lonchanick <diego.arroyo2462050@gmail.com>
2025-08-23 10:37:28 -0400

    exercise 3.12 done: Create a cloud-based MongoDB database for the phonebook application with MongoDB Atlas

---
+++ part2/backend-express/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const collectionName = "person";
+const url = `mongodb+srv://Lonchanick:${password}@fullstackopen.okvdrf9.mongodb.net/${collectionName}?retryWrites=true&w=majority&appName=fullstackOpen`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+});
+
+const Person = mongoose.model("Person", noteSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("Phone Book:");
+    result.forEach((p) => console.log(p));
+    mongoose.connection.close();
+  });
+
+  return;
+}
+
+const person = new Person({
+  name: process.argv[3].toString(), //name and..
+  phone: process.argv[4].toString(), //phone number
+});
+
+person.save().then((result) => {
+  console.log(`Added: ${person}`);
+  mongoose.connection.close();
+});
