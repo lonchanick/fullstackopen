ebd7997cbc5cfdc4851a94f4b3ad4894b4fb4a20
lonchanick <diego.arroyo2462050@gmail.com>
2025-09-18 19:39:18 -0300

    going for .env file xd

---
+++ part3/Backend/Models/phonebook.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+mongoose.set("strictQuery", false); 
+const url = process.env.MONGODB_URI;
+
+console.log("Connecting to ", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Succefull connection!");
+  })
+  .catch((err) => {
+    console.log("Something went wrong", err.message);
+  });
+
+  const noteSchema = new mongoose.Schema({
+    name: String,
+    number: String
+  });
+
+  noteSchema.set('toJSON', {
+    transform: (document, returnedObject)=>{
+        returnedObject.id = returnedObject._id;
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+  }) 
+  module.exports = mongoose.model('phonebook',noteSchema);
