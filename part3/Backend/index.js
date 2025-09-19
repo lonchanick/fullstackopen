807809ec4825c8f2c1fe21a071b24a35ad9fd02c
lonchanick <diego.arroyo2462050@gmail.com>
2025-09-19 15:44:11 -0300

    done

--- part3/Backend/index.js
+++ part3/Backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const phonebook = require("./Models/phonebook");
+const { default: phone } = require("../myapp/src/services/phone");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 morgan.token("req-body", (req) =>
@@ -14,72 +17,19 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 //home page
 app.get("/api", (request, response) => {
-  response.send("<h1>Home Page</h1>");
-});
-//***********************CURRENT*******************************/
-//get request to create a new record, working.
-// app.get('/api/createAll', (request, response)=>{
-//   const family = [
-//     {name: "Dayana Intriago", number: "0000000001"},
-//     {name: "Diego Arroyo", number: "0000000002"},
-//     {name: "Agustin Arroyo", number: "0000000003"}]
+  response.send("<h1>Contact API Interface - Home Page</h1>");
+}); 
 
-//   // const newPhonebook = new Phonebook(family);
-//   Phonebook.insertMany(family)
-//   .then(()=>{
-//     response.json({response: "Mocking data"});
-//   });
-// });
-//***********************CURRENT*******************************/
-
-//get persons
+//get contact
 app.get("/api/contacts", (request, response) => {
   phonebook.find({}).then((contacts) => {
     response.json(contacts);
   });
 });
 
-//get info
-app.get("/api/info", (request, response) => {
-  const today = new Date();
-  const formatted = today.toLocaleDateString("en-US", {
-    weekday: "long", // e.g., Saturday
-    year: "numeric",
-    month: "long", // e.g., August
-    day: "numeric",
-  });
-
-  const contactsLength = phonebook.find({}).then((contacts) => {
-    const info = `${formatted} --- Phone Book has info for ${contacts.length} people.`;
-    response.status(200).json(info);
-  });
-});
-
+//get contact by id
 app.get("/api/contact/:id", (request, response) => {
   const id2 = request.params.id;
   phonebook.findById(id2)
@@ -88,6 +38,19 @@ app.get("/api/contact/:id", (request, response) => {
   })
  
 });
+
+//create new contact in mongodb
+app.post('/api/contacts',(request, response)=>{
+  const body = request.body;
+  if(!body) return response.status(400).json({"error":"missing body"});
+  const newContact = new phonebook( {name: body.name, number: body.number});
+
+  newContact.save()
+  .then(result => {
+    console.log(result);
+    response.json(result);
+  });
+})
 
 
 const PORT = process.env.PORT;
