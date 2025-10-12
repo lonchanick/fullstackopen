7f26d4565d475f58b9114d5e09424565cd6b1f87
lonchanick <diego.arroyo2462050@gmail.com>
2025-10-12 19:27:12 -0300

    Implement GET and DELETE endpoints for notes (exercises 3.15-3.18)

--- lab/backend-express/index.js
+++ lab/backend-express/index.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const Note = require('./models/note')
 const app = express();
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
  
  
 
@@ -18,18 +18,21 @@ app.get("/api/notes", (req, resp) => {
 });
 
 //get note by id
-app.get("/api/notes/:id", (request, resp) => {
-  const id = req.params.id;
-  const note = notes.find((n) => n.id === id);
-
-  if (note) return resp.status(200).json(note);
-
-  return resp.status(404).json({ response: "Element not found" });
+app.get("/api/notes/:id", (request, response,next) => {
+  const id = request.params.id;
+  Note.findById(id).then(note => {
+    if(note) return response.json(note);
+    else return response.status(400).json({error: "Note id does not exist!"});
+    
+  }).catch(err => {
+    next(err);
+    // console.log("Something went wrong");
+    // response.json({error: "Something went wrong DUDE", details: err});
+  })
 });
 
 //set new note
 app.post("/api/notes", (request, response) => {
-  console.log(" >>>>>>>> setting new note");
   const body = request.body;
   if(!body) return response.status(400).json({"error":"missing body"});
   const newNote = new Note( {content: body.content, important: body.important || false});
@@ -38,15 +41,42 @@ app.post("/api/notes", (request, response) => {
 });
 
 //delete note
-app.delete("/api/notes/:id", (req, resp) => {
-  const id = req.params.id;
-
-  if (!id) return resp.status(400).json({ response: "Bad request dude!" });
-  
+app.delete("/api/notes/:id", (req, resp,next) => {
+  const id = req.params.id; 
   Note.findByIdAndDelete(id).then((response)=>{
     return resp.status(200).end();
-  }) 
+  }).catch(err)
+  {
+    next(err);
+  }
 });
+
+app.put("/api/notes/:id",(request, response, next)=>{
+  const {content, important} = request.body;
+  
+  Note.findById(request.params.id).then(note => {
+    if(!note) return response.status(404).end();
+    
+    note.content = content;
+    note.important = important;
+
+    note.save().then(updatedNote => {
+      return response.json(updatedNote);
+    })
+
+  }).catch(error => next(error));
+})
+
+
+const errorHandler = (error, request, response, next)=>{
+  console.log("ERROR DETAILS:\n",error);
+  if(error.name === "CastError") return response.status(400).json({error: "malformatted Id"});
+  next(error);
+}
+//Note that the error-handling middleware has to be the last loaded middleware, also all the routes
+// should be registered before the error-handler!
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- lab/myapp/src/components/Note.jsx
+++ lab/myapp/src/components/Note.jsx
@@ -2,7 +2,8 @@ const Note = ({ note,toggleImportance, deleteNote}) => {
   const buttonText = note.important ? "Make not important" : "Make important";
   return (
     <li className="note" >
-      {note.content.slice(0, 15)}
+      {`content: ${note.content.slice(0, 15)}`}<br></br>
+      {`id: ${note.id}`}
       <button onClick={()=> toggleImportance(note._id)}>{buttonText}</button>
       <button onClick={()=> deleteNote(note.id)}>Delete</button>
     </li>
