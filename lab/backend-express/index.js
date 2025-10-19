412013eedd6094c1c679b2c3b852abee11f0b70c
lonchanick <diego.arroyo2462050@gmail.com>
2025-10-19 12:09:52 -0300

    lab for Part 3: Validation and ESLint

--- lab/backend-express/index.js
+++ lab/backend-express/index.js
@@ -1,78 +1,93 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const Note = require('./models/note')
+const Note = require("./models/note");
 const app = express();
 
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
- 
- 
 
 //get all notes
 app.get("/api/notes", (req, resp) => {
   Note.find({}).then((result) => {
-    resp.json(result); 
-  }); 
+    resp.json(result);
+  });
 });
 
 //get note by id
-app.get("/api/notes/:id", (request, response,next) => {
+app.get("/api/notes/:id", (request, response, next) => {
   const id = request.params.id;
-  Note.findById(id).then(note => {
-    if(note) return response.json(note);
-    else return response.status(400).json({error: "Note id does not exist!"});
-    
-  }).catch(err => {
-    next(err);
-    // console.log("Something went wrong");
-    // response.json({error: "Something went wrong DUDE", details: err});
-  })
+  Note.findById(id)
+    .then((note) => {
+      if (note) return response.json(note);
+      else
+        return response.status(400).json({ error: "Note id does not exist!" });
+    })
+    .catch((err) => {
+      next(err);
+      // console.log("Something went wrong");
+      // response.json({error: "Something went wrong DUDE", details: err});
+    });
 });
 
 //set new note
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-  if(!body) return response.status(400).json({"error":"missing body"});
-  const newNote = new Note( {content: body.content, important: body.important || false});
-  newNote.save();
-  return response.status(200).json(newNote);
+  // if(!body) return response.status(400).json({"error":"missing body"});
+  const newNote = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+  newNote.save().then(response => {
+    return response.status(200).json(newNote);
+  }).catch((err) => next(err, response));
+  
 });
 
 //delete note
-app.delete("/api/notes/:id", (req, resp,next) => {
-  const id = req.params.id; 
-  Note.findByIdAndDelete(id).then((response)=>{
-    return resp.status(200).end();
-  }).catch(err)
+app.delete("/api/notes/:id", (req, resp, next) => {
+  const id = req.params.id;
+  Note.findByIdAndDelete(id)
+    .then((response) => {
+      return resp.status(200).end();
+    })
+    .catch(err);
   {
     next(err);
   }
 });
 
-app.put("/api/notes/:id",(request, response, next)=>{
-  const {content, important} = request.body;
-  
-  Note.findById(request.params.id).then(note => {
-    if(!note) return response.status(404).end();
-    
-    note.content = content;
-    note.important = important;
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-    note.save().then(updatedNote => {
-      return response.json(updatedNote);
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) return response.status(404).end();
+
+      note.content = content;
+      note.important = important;
+
+      note.save().then((updatedNote) => {
+        return response.json(updatedNote);
+      });
     })
+    .catch((error) => next(error));
+});
 
-  }).catch(error => next(error));
-})
+const errorHandler = (error, request, response, next) => {
+  console.error("ERROR DETAILS:\n", error.message);
+  if (error.name === "CastError")
+  {
+    return response.status(400).json({ error: "malformatted Id" });
+  }else if(error.name === "ValidationError")
+  {
+    return response.status(400).json({error: error.message})
+  }
 
-
-const errorHandler = (error, request, response, next)=>{
-  console.log("ERROR DETAILS:\n",error);
-  if(error.name === "CastError") return response.status(400).json({error: "malformatted Id"});
   next(error);
-}
+};
+
 //Note that the error-handling middleware has to be the last loaded middleware, also all the routes
 // should be registered before the error-handler!
 // this has to be the last loaded middleware, also all the routes should be registered before this!
