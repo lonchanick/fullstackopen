0c5e28ccbd8238a85fbe20cb10ffcd94e4fa5009
lonchanick <diego.arroyo2462050@gmail.com>
2025-11-01 21:10:09 -0300

    middlewareseses!

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 const config = require('./utils/config');
 const blogRouter = require('./controllers/blog')
-const{info, error} = require('./utils/logger');
+const logger = require('./utils/logger');
+const middleWare = require('./utils/middleware');
 
 const app = express();
 
-app.use(cors());
-app.use(express.json())
-app.use('/api/blogs',blogRouter);
-
-info('connecting to', config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI);
 mongoose.connect(config.MONGODB_URI)
 .then((response)=>{
-  info("Succefully connected ...");
+  logger.info("Succefully connected ...");
 }).catch(err => error("something went wrong...!!"));
 
+
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json())
+app.use(middleWare.requestLogger);
+app.use('/api/blogs',blogRouter);
+app.use(middleWare.unknownEndpoint);
+app.use(middleWare.errorHandler);
 
 module.exports = app;
