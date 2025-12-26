4bd4365d0014e62d7630a9e0b0eeeb1d600565c1
lonchanick <diego.arroyo2462050@gmail.com>
2025-12-25 21:48:53 -0300

    token implementation done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const notesRouter = require('./controllers/notes')
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/usersController');
-const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware');
+const loginRouter = require('./controllers/login');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- controllers/login.js
+++ controllers/login.js
@@ -1,1 +1,28 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const loginRoute = require("express").Router();
+const bcrypt = require("bcrypt");
+
+loginRoute.post("/", async (request, response) => {
+  const { username, password } = request.body;
+  const user = await User.findOne({ username });
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(passwordCorrect && user))
+    return response.status(401).json({ error: "wrong password or username" });
+
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  }
+  
+  const token = jwt.sign(userForToken,process.env.SECRET);
+
+  response.status(200).send({token, username: username, name: user.name})
+
+});
+
+module.exports = loginRoute;
